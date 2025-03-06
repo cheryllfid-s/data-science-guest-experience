09436b836b75dd88e09e5e1699d3c3b715af7b78
xuanastasia <huiyxyx@gmail.com>
2025-03-07 00:21:02 +0800

    setting visualisation style

--- Scripts/Subgroup_A/guest_satisfaction.py
+++ Scripts/Subgroup_A/guest_satisfaction.py
@@ -3,5 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
+from matplotlib.ticker import PercentFormatter
+
+# Setting up visualization style
+sns.set(style="whitegrid")
+plt.rcParams['figure.figsize'] = (12, 6)
+
 
 
