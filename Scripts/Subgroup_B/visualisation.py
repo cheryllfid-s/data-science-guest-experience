39545fb8dc0dff08958de314e499f95cd576c137
KeaganLai <zkeaganlai@gmail.com>
2025-03-07 13:36:29 +0800

    Added visualisation file

---
+++ Scripts/Subgroup_B/visualisation.py
@@ -0,0 +1,4 @@
+# Referring to google forms, we can see that Revenge of the Mummy (ROTM) > Cylon > Transformers were the most popular rides, "Thrilling"
+# However, these 3 rides also had the longest wait times among all the rides 
+# Additionally, Transformers, Sesame Street, and ROTM had the most complaints (18,18,13 out of 210 respectively), "Boring", "Too scary (drop)"
+# But the average consensus on the wait times being worth it was a meh 3/5
