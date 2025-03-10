126328f0469f4d447663ed7f602b7319fa8c3c3e
KeaganLai <zkeaganlai@gmail.com>
2025-03-10 20:13:51 +0800

    Shift response.csv to correct folder, ready for work using pandas

--- Scripts/Subgroup_B/visualisation.py
+++ Scripts/Subgroup_B/visualisation.py
@@ -1,4 +1,8 @@
 # Referring to google forms, we can see that Revenge of the Mummy (ROTM) > Cylon > Transformers were the most popular rides, "Thrilling"
 # However, these 3 rides also had the longest wait times among all the rides 
 # Additionally, Transformers, Sesame Street, and ROTM had the most complaints (18,18,13 out of 210 respectively), "Boring", "Too scary (drop)"
-# But the average consensus on the wait times being worth it was a meh 3/5
+# But the average consensus on the wait times being worth it was a meh 3/5'
+
+import pandas as pd
+uss_df = pd.read_csv('USS_Responses.csv')
+print(uss_df.head())
