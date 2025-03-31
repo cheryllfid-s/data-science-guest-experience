15fca5d27dd1d3897fd2ff7d8843a16109a6a67b
cheryllfid-s <e1075554@u.nus.edu>
2025-04-01 04:02:16 +0800

    Update main.py
    to check if everything runs well after merging the branches and main_A and main_B are done

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,10 +2,10 @@ from Subgroup_A.main_A import mainA
 from Subgroup_B.main_B import mainB
 
 def run_all():
-    print("Running Subgroup A Analysis...")
+    print("\nRunning Subgroup A Analysis")
     main_A()
 
-    print("\nRunning Subgroup B Analysis...")
+    print("\nRunning Subgroup B Analysis")
     main_B()
 
 if __name__ == "__main__":
