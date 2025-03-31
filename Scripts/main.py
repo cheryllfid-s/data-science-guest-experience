ebee83a806f9a925b5c2cf4283a4a1a00887034d
cheryllfid-s <e1075554@u.nus.edu>
2025-04-01 04:00:23 +0800

    Update main.py
    to see if this runs well once main_A and main_B files are done

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,1 +1,12 @@
+from Subgroup_A.main_A import mainA
+from Subgroup_B.main_B import mainB
 
+def run_all():
+    print("Running Subgroup A Analysis...")
+    main_A()
+
+    print("\nRunning Subgroup B Analysis...")
+    main_B()
+
+if __name__ == "__main__":
+    run_all()
