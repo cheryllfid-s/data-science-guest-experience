e76547bb69e788aee44737209a3c382108be450d
jolenelxr <jolene.limxr@gmail.com>
2025-03-31 21:20:52 +0800

    edited print statement

--- Scripts/Subgroup_A/main_A.py
+++ Scripts/Subgroup_A/main_A.py
@@ -9,13 +9,13 @@ def mainA():
     
     tivoli_g, attendance_df, covid, negative_att = prepare_tivoli_data()
     GuestJourneyAnalysis_obj = GuestJourneyAnalysis(tivoli_g, attendance_df, covid, negative_att)
-    Print("Question 3: Guest Journey Patterns")
+    print("Question 3: Guest Journey Patterns")
     GuestJourneyAnalysis_obj.run_guestjourneyanalysis()
 
+    print("Question 4: Impact of Marketing Strategies on Guest Behaviour")
     q4_df_reviews = q4_prepare_reviews_data()
     q4_df_events = q4_prepare_events_data()
     USSPromoAnalysis_obj = USSPromoAnalysis(q4_df_reviews, q4_df_events)
-    print("Question 4: Impact of Marketing Strategies on Guest Behaviour")
     USSPromoAnalysis_obj.run_promo_events_analysis()
 
 if __name__ == "__main__":
