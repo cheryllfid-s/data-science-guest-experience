dd747976a245f0191ea854e697b5fcc40c696b5a
jolenelxr <122447429+jolenelxr@users.noreply.github.com>
2025-03-31 17:54:35 +0800

    updated main_A.py for qn4

--- Scripts/Subgroup_A/main_A.py
+++ Scripts/Subgroup_A/main_A.py
@@ -7,5 +7,11 @@ def mainA():
     Print("Question 3: Guest Journey Patterns")
     GuestJourneyAnalysis_obj.run_guestjourneyanalysis()
 
+    q4_df_reviews = prepare_reviews_data()
+    q4_df_events = prepare_events_data()
+    USSPromoAnalysis_obj = USSPromoAnalysis(q4_df_reviews, q4_df_events)
+    print("Question 4: Impact of Marketing Strategies on Guest Behaviour")
+    USSPromoAnalysis_obj.run_promo_events_analysis()
+
 if __name__ == "__main__":
     mainA()
