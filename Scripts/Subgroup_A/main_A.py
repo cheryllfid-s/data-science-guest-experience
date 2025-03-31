06f9e1938d94c1a31f322fbd546032f80259b755
cheryllfid-s <e1075554@u.nus.edu>
2025-03-31 17:06:03 +0800

    Update main_A.py
    added a text before the Questions to better distinguish it later

--- Scripts/Subgroup_A/main_A.py
+++ Scripts/Subgroup_A/main_A.py
@@ -4,6 +4,7 @@ from guest_journey import GuestJourneyAnalysis
 def mainA():
     tivoli_g, attendance_df, covid, negative_att = prepare_tivoli_data()
     GuestJourneyAnalysis_obj = GuestJourneyAnalysis(tivoli_g, attendance_df, covid, negative_att)
+    Print("Question 3: Guest Journey Patterns")
     GuestJourneyAnalysis_obj.run_guestjourneyanalysis()
 
 if __name__ == "__main__":
